d4ccb686eea2592ebea1fe00b87842c3e1b80621
Shabhaizafar <zafarShabhai03@gmail.com>
2024-06-24 11:00:00 +0530

    forloop

---
+++ Loops/for.py
@@ -0,0 +1,113 @@
+# li = [11,12,13,14,1,6,7,8,0]
+
+# 1) i = item
+# for i in li : 
+#     print(i,end='')
+
+
+# 2)  range()      i = index
+# for i in range(0,len(li)):
+#     print("Index : ",i,"Value :",li[i])
+
+
+# 3)
+# for i in range(len(li)):
+#     print("Index : ",i,"Value :",li[i])
+
+# n1 = 101
+# n2 = 110
+
+# for i in range(n2,n1-1,-2):
+#     print(i)
+
+
+# for i in range(n1,n2,2):
+#     print(i)
+
+# 4)
+# for i in range(0,10):
+#     print(i)
+# else :
+#     print("Completed")
+
+# for i in range(0,10):
+#     pass        #ignore this Block
+
+# 5)
+# for i in range(0,10):
+#     print(i)
+#     if(i==5):
+#         break    # break down stop
+# else :
+#     print("Completed")
+
+# 6)
+# for i in range(0,10):
+#     if(i==5):
+#         continue    # skip 
+#     print(i)
+# else :
+#     print("Completed")
+
+
+
+# #########################################
+print("i j")
+# for i in range(0,3):
+#     for j in range(0,3):
+#         print(i,j)
+
+# for i in range(0,3):
+#     for j in range(0,3):
+#         if(i==1):
+#             break
+#         print(i,j)
+
+# for i in range(0,3):
+#     for j in range(0,3):
+#         if(j==1):
+#             break
+#         print(i,j)
+
+# for i in range(0,3):
+#     for j in range(0,3):
+#         if(j==1):
+#             continue
+#         print(i,j)
+
+# for i in range(0,3):
+#     for j in range(0,3):
+#         if(i==1):
+#             continue
+#         print(i,j)
+
+'''
+Sure! Here is a list of practice questions to help you understand and practice using `break`, `continue`, and `pass` in Python.
+
+### Break Practice Questions
+
+1. Write a program that iterates through numbers from 1 to 100 and prints each number. Stop the loop if a number divisible by both 3 and 5 is found.
+2. Create a list of strings. Write a loop that iterates through the list and prints each string until it encounters a string that contains the word "stop".
+3. Write a loop that iterates through a list of numbers and prints each number. Break the loop if you encounter a negative number.
+4. Write a program that takes user input in a loop. If the user types "exit", break the loop.
+5. Write a program that generates random numbers between 1 and 10. Stop the loop when a 7 is generated.
+
+### Continue Practice Questions
+
+1. Write a loop that iterates through numbers from 1 to 20. Print each number except those that are divisible by 4.
+2. Create a list of strings. Write a loop that prints each string except those that contain the letter "a".
+3. Write a loop that iterates through a list of numbers and prints each number except the number 5.
+4. Write a program that asks the user to enter numbers in a loop. Skip the numbers that are less than 10.
+5. Write a loop that iterates through the characters in a string and prints each character except spaces.
+
+### Pass Practice Questions
+
+1. Write a loop that iterates through numbers from 1 to 10 and uses the `pass` statement inside the loop.
+2. Create a list of numbers. Write a loop that iterates through the list and uses `pass` for each iteration.
+3. Write a function that uses `pass` as the function body.
+4. Write a conditional statement that checks if a number is positive, negative, or zero, and uses `pass` for one of the conditions.
+5. Write a class definition with a method that uses `pass` as the method body.
+
+These questions should give you a good mix of scenarios to practice using `break`, `continue`, and `pass` in Python.
+
+'''
---
+++ unzip.py
@@ -0,0 +1,30 @@
+# # var = "google"
+# # mylist = []
+# # print(var)
+# # for i in var:
+# #     result = zip(i,str(var.count(i)))
+# #     newone = list(zip(*result))      
+# #     print(newone)
+
+
+
+# # ziped  = [('Akshat', 1), ('Bro', 2), ('is', 3), ('Placed', 4)]
+
+# # unziped = list(zip(*ziped))
+# # print(unziped)
+
+
+# myList = [1,2,3,4,5,6]
+# mystr ="ZafarA"
+
+# newzip = zip(myList,mystr)
+# # for i in newzip :
+# #     print(i)
+
+# unziped = list(zip(*newzip))
+# print(unziped)
+
+
+# new2 = zip(unziped[0],unziped[1])
+# for i in new2:
+#     print(i)
